140cf634c64ea69eeb7763d289b6921cdcc3ceaa
uslumt <81621143+uslumt@users.noreply.github.com>
2022-05-09 19:09:12 +0200

    Update RandomForest.m
    RF implementation

--- RandomForest.m
+++ RandomForest.m
@@ -1,7 +1,6 @@
 %% Regression Tree Implementation %%
 
 tree = fitrtree(train_features, train_labels, 'ResponseName','Y')
-
 Tree_predicted = predict(tree, [test_features]);
 Tree_re_sub_error = resubLoss(tree)
 
@@ -18,20 +17,24 @@ ylabel("test labels");
 legend('Test Lables', 'Predicted Labels')
 hold off;
 
-% There is a bug while using the loss funtion for Mean Squared error
-% calculation
+% There is a bug while using the loss funtion for Mean Squared error calculation
 % L = loss(tree, test_features, 'Capacity(State of Charge)')
 
 %% Enseble of regression Trees(Random forest)
 
 t = templateTree('Surrogate','on');
 RF = fitrensemble(Data_train,'Capacity(State of Charge)', ...
-    'OptimizeHyperparameters',{'NumLearningCycles','LearnRate','MaxNumSplits'})
+    'OptimizeHyperparameters',{'NumLearningCycles','LearnRate','MaxNumSplits'});
 RF_predicted = predict(RF, [Data_test]);
+RF_root_mean_squared_error = sqrt(immse(RF_predicted, Data_test.("Capacity(State of Charge)")));
+RF_re_sub_error = resubLoss(RF);
 
-V1 = RF_predicted;
-V2 = Data_test.("Capacity(State of Charge)");
+figure;
+plot(1:length(Data_test.("Capacity(State of Charge)")), [Data_test.("Capacity(State of Charge)")], '.');
+hold on;
+plot(1:length([RF_predicted]), [RF_predicted], '.');
+xlabel("Index")
+ylabel("test labels")
+title( sprintf("Random Rorest Regression (RMSE : %f)", RF_root_mean_squared_error))
+hold off;
 
-RF_root_mean_squared_error = sqrt(mean((V1-V2).^2))
-
-RF_re_sub_error = resubLoss(RF)
